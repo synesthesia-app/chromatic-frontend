b4baec4180b0c8db0c54a297cfccd8b139343215
Alex Blair <bclan11@gmail.com>
2022-05-24 15:29:28 -0700

    working on eyedropper functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import * as Tone from 'tone';
+import { EyeDropper } from 'react-eyedrop';
+import { useState } from 'react';
 
 export default function App() {
+  const [pickedColor, setPickedColor] = useState('#bada55');
+
+  function getColor({ rgb, hex }) {
+    setPickedColor(hex);
+    console.log(rgb, hex);
+  }
+
   const handleClick = () => {
     //create a synth and connect it to the main output (your speakers)
     const synth = new Tone.Synth().toDestination();
@@ -11,5 +20,29 @@ export default function App() {
     synth2.triggerAttackRelease('E4', '4n');
   };
 
-  return <button onClick={handleClick}>Play sound</button>;
+  return (
+    <>
+      <button onClick={handleClick}>Play sound</button>
+      <EyeDropper onChange={getColor} />
+      <div
+        style={{ height: '4rem', backgroundColor: 'yellow' }}
+        className="yellow"
+      >
+        Yellow
+      </div>
+      <div style={{ height: '4rem', backgroundColor: 'blue' }} className="blue">
+        Blue
+      </div>
+      <div style={{ height: '4rem', backgroundColor: 'red' }} className="red">
+        Red
+      </div>
+      <div
+        style={{
+          height: '4rem',
+          width: '100%',
+          backgroundColor: `${pickedColor}`,
+        }}
+      ></div>
+    </>
+  );
 }
