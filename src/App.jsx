1d6ba47406b14018a64c41c37c484c4d0bbc1ea0
Philippe Ngom <philngom@gmail.com>
2022-05-24 13:03:19 -0700

    tone.js proof of concept demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import * as Tone from 'tone';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const handleClick = () => {
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+    const synth2 = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '4n');
+    synth2.triggerAttackRelease('E4', '4n');
+  };
+
+  return <button onClick={handleClick}>Play sound</button>;
 }
