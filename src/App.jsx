0f6e2ce3441b494c86f786f2f89db85b4e4175e0
Clayton Knapp <csjknapp@gmail.com>
2022-05-27 18:01:05 -0700

    Added router to app, linked login to oauth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, NavLink, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Link, NavLink, Route, Redirect } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './views/Main/Main';
 import Login from './views/Login/Login';
