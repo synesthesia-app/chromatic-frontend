30b07ea821e1926082c420bc5b1d34d9f2a67b69
Alex Blair <bclan11@gmail.com>
2022-06-02 17:04:45 -0700

    click saved palette populates palette array and palette name. Can change name but saves as new saved palette

--- src/components/SavedPalette/SavedPalette.jsx
+++ src/components/SavedPalette/SavedPalette.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import styles from './SavedPalette.css';
+import { useColorNote } from '../../context/ColorNoteProvider';
 
 export default function SavedPalette({ name, swatchArr }) {
+  const { setColorPalette, setPaletteName } = useColorNote();
   const [palette, setPalette] = useState([]);
+
   useEffect(() => {
     const parseData = () => {
       const parsedPalette = JSON.parse(swatchArr);
@@ -11,29 +14,30 @@ export default function SavedPalette({ name, swatchArr }) {
     parseData();
   }, []);
 
+  const handlePaletteClick = () => {
+    setPaletteName(name);
+    setColorPalette(palette);
+  };
+
   return (
-    <>
-      <section className={styles.paletteContainer}>
-        <p className={styles.paletteName}>{name}</p>
-        <div className={styles.buttonAndPaletteContainer}>
-          <img src="./trash@2x.png" className={styles.deletePaletteButton} />
-          <div className={styles.swatchContainer}>
-            {palette?.map((swatch, i) => {
-              return (
-                <div
-                  key={swatch.name + i}
-                  className={styles.swatch}
-                  style={{
-                    backgroundColor: `hsl(${swatch.hue}, ${swatch.sat}%,${swatch.light}%)`,
-                  }}
-                  title={`${swatch.name}`}
-                  onClick={() => handleSwatchClick(swatch)}
-                ></div>
-              );
-            })}
-          </div>
+    <section title={`${name}`} className={styles.paletteContainer}>
+      <p className={styles.paletteName}>{name}</p>
+      <div className={styles.buttonAndPaletteContainer}>
+        <img src="./trash@2x.png" className={styles.deletePaletteButton} />
+        <div className={styles.swatchContainer} onClick={handlePaletteClick}>
+          {palette?.map((swatch, i) => {
+            return (
+              <div
+                key={swatch.name + i}
+                className={styles.swatch}
+                style={{
+                  backgroundColor: `hsl(${swatch.hue}, ${swatch.sat}%,${swatch.light}%)`,
+                }}
+              ></div>
+            );
+          })}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
