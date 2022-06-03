3ed28a5cac0419cd8e3e3af8900e2c3b1d3ef6f9
armaBurton <armanarma@armaburton.com>
2022-06-02 19:16:01 -0700

    guests have default images to pick through

---
+++ src/components/ImageScroller/defaultArray.js
@@ -0,0 +1,157 @@
+export const defaultArray = [
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217481/b6dlbmq8dak2hffmlsom.jpg',
+    publicId: 'b6dlbmq8dak2hffmlsom',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217514/ll3fqamw0kqaek6inyke.jpg',
+    publicId: 'll3fqamw0kqaek6inyke',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217539/o2qixynzv4vx2kzbz3g1.jpg',
+    publicId: 'o2qixynzv4vx2kzbz3g1',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217592/eqse0fjqwfod5gkunqp2.jpg',
+    publicId: 'eqse0fjqwfod5gkunqp2',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217651/elbaqlsld8tf6exenn2b.jpg',
+    publicId: 'elbaqlsld8tf6exenn2b',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217667/lbwypid2kdla61xqasv8.jpg',
+    publicId: 'lbwypid2kdla61xqasv8',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217699/lhjulrna7kvdvnp9kzp4.jpg',
+    publicId: 'lhjulrna7kvdvnp9kzp4',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217725/cpktypyklpb49onn39p3.jpg',
+    publicId: 'cpktypyklpb49onn39p3',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217765/kzivfgbixgwkyzk5ieo2.jpg',
+    publicId: 'kzivfgbixgwkyzk5ieo2',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217782/yrgb3uhjkonzieyhiewh.jpg',
+    publicId: 'yrgb3uhjkonzieyhiewh',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217823/uneoifkboiduwgz87rny.jpg',
+    publicId: 'uneoifkboiduwgz87rny',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654217858/mobdnemxdtbdmmjpvybk.jpg',
+    publicId: 'mobdnemxdtbdmmjpvybk',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218657/w07lb4ly8jertd04fwxf.jpg',
+    publicId: 'w07lb4ly8jertd04fwxf',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218672/ky0jis4cxwodytfxrkus.jpg',
+    publicId: 'ky0jis4cxwodytfxrkus',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218697/oeynwzdhhcrztdgl7k3h.jpg',
+    publicId: 'oeynwzdhhcrztdgl7k3h',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218717/gctcoooz1yvozy9mjgkw.jpg',
+    publicId: 'gctcoooz1yvozy9mjgkw',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218775/eot6vn2wg0wf1sygrilu.jpg',
+    publicId: 'eot6vn2wg0wf1sygrilu',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218804/jo4tv7e6vsbyo0qujdlr.png',
+    publicId: 'jo4tv7e6vsbyo0qujdlr',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218830/ktpb8al0evecrvh0vnad.jpg',
+    publicId: 'ktpb8al0evecrvh0vnad',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218872/bxlbpk4pagayfaufph8h.jpg',
+    publicId: 'bxlbpk4pagayfaufph8h',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218930/xan98ggkqn61ibktsxdf.jpg',
+    publicId: 'xan98ggkqn61ibktsxdf',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218952/sbvrilouaaek4blywm7n.jpg',
+    publicId: 'sbvrilouaaek4blywm7n',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654218982/unjcbtss2ddoeyiuzxer.jpg',
+    publicId: 'unjcbtss2ddoeyiuzxer',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219007/pinnen8n7hfryqvfu3be.jpg',
+    publicId: 'pinnen8n7hfryqvfu3be',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219034/bf5dql45ipx9afzjejaa.jpg',
+    publicId: 'bf5dql45ipx9afzjejaa',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219052/tzjphjsqqzicgghqif0q.jpg',
+    publicId: 'tzjphjsqqzicgghqif0q',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219115/re6alnn9tlayk3pbruux.jpg',
+    publicId: 're6alnn9tlayk3pbruux',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219125/jvbj3f7ezp3uvktcab3w.jpg',
+    publicId: 'jvbj3f7ezp3uvktcab3w',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219149/m8momcdrftomcftayi8t.jpg',
+    publicId: 'm8momcdrftomcftayi8t',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219198/mjsrwgmzhocfolqhi8nw.jpg',
+    publicId: 'mjsrwgmzhocfolqhi8nw',
+  },
+  {
+    imageName:
+      'http://res.cloudinary.com/de3xditoa/image/upload/v1654219307/s5qrwtics7qqcmsvx4dx.jpg',
+    publicId: 's5qrwtics7qqcmsvx4dx',
+  },
+];
